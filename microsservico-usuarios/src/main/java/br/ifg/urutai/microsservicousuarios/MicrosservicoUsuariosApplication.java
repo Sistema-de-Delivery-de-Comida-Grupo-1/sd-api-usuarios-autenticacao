e56787700c22036ad54ebec9bd9e8441d584b94c
Cristian <cristianmarfer14@gmail.com>
2026-05-25 09:00:42 -0300

    Criação dos módulos dos microserviços

--- microsservico-usuarios/src/main/java/br/ifg/urutai/microsservicousuarios/MicrosservicoUsuariosApplication.java
+++ microsservico-usuarios/src/main/java/br/ifg/urutai/microsservicousuarios/MicrosservicoUsuariosApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicrosservicoUsuariosApplication {
 
-    //
+    //kkk
     public static void main(String[] args) {
         SpringApplication.run(MicrosservicoUsuariosApplication.class, args);
     }
